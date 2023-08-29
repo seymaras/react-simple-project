a6accc086588d8e84ad8dea39e63f6b2d5a9f591
Asiye Şeyma Aras <seyma02aras@gmail.com>
2023-08-29 12:46:08 +0300

    input, button css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+function Input() {
+  const [inputYazi, setInputYazi] = useState("");
+
+  const inputYaziYenileme = (event) => {
+    setInputYazi(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    alert(inputYazi);
+  };
+
   return (
-    <>
-      <input type="text" />
-      <button>Metin</button>
-    </>
+    <div class="orta-div">
+      <p>
+        Simple <strong>Input</strong> <i>Alert</i>
+      </p>
+      <input
+        type="text"
+        value={inputYazi}
+        onChange={inputYaziYenileme}
+        placeholder="Type something..."
+      />
+
+      <button className="Button" onClick={handleButtonClick}>
+        Gönder
+      </button>
+    </div>
   );
 }
 
-export default App;
+export default Input;
